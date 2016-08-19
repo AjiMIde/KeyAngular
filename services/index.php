47ac6340f66bd3dcfef30a2fdd08fdf05d636f64
AjiMIde <adele513900383@gmail.com>
2016-08-19 17:43:29 +0800

    增加了3个Demo：form/$http/ng-repeat

---
+++ services/index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User        : Aij
+ * DateTime    : 2016/8/19-16:44
+ * Modified    : 2016/8/19-16:44
+ * Description :
+ */
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : "";
+
+if($action == 'getData'){
+    $array = array(
+        array(
+            'name'=>'26 Development',
+            'count'=>20,
+            'square'=>'400m'
+        ),
+        array(
+            'name'=>'25 Test',
+            'count'=>11,
+            'square'=>'100m'
+        ),
+        array(
+            'name'=>'25 Caiwu',
+            'count'=>6,
+            'square'=>'50m'
+        )
+    );
+    echo json_encode($array);
+}elseif($action == 'postData'){
+    $params = json_decode(file_get_contents('php://input'),true);
+    echo json_encode($params);
+}else{
+    echo 'no data';
+}
